8f931a976a6e7b76003c52968466a915d658dd0f
Jeremy Nelson <jermnelson.gmail.com>
2013-12-12 16:51:15 -0700

    Contining to work on presentation

--- server.py
+++ server.py
@@ -4,11 +4,35 @@ __author__ = "Jeremy Nelson"
 __license__ = 'GPL v2'
 __copyright__ = '(c) 2013 by Jeremy Nelson'
 
+import json
+import os
 from flask import Flask, g, redirect, render_template
 
 URL_PREFIX = ''
 
 app = Flask(__name__)
+
+RESOURCES = {'articles_books': [],
+             'software': [],
+             'websites': []}
+
+def load_resources(section, names):
+    for name in names:
+        RESOURCES[section].append(
+            json.load(
+                open(
+                    os.path.join('static',
+                                 'js',
+                                 '{0}.json'.format(name)))))
+
+load_resources('articles_books',
+               ['linking-things-on-the-web',
+                'understanding-marc-bibliographic'])
+
+load_resources('websites',
+               ['bibframe',
+                'jinja',
+                'rdfa-lite'])
 
 @app.route('{0}/badge'.format(URL_PREFIX))
 def badge():
@@ -20,7 +44,8 @@ def home():
 
 @app.route('{0}/resources'.format(URL_PREFIX))
 def resources():
-    return render_template('resources.html')
+    return render_template('resources.html',
+                           resources=RESOURCES)
 
 if __name__ == '__main__':
     host = '0.0.0.0'
